cac7f84aba412a00d4d1be6ef06304a3380ca8c4
adam <adam@moemail.app>
2026-06-15 12:01:57 +0800

    refactor: add tft_display_toggle_page and tft_display_get_i2c_bus to header

--- main/tft_display.h
+++ main/tft_display.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "board.h"
 #if HAS_TFT_DISPLAY
+#include "driver/i2c_master.h"
 #include "esp_lcd_panel.h"
 esp_lcd_panel_handle_t tft_display_get_panel(void);
 void tft_display_freeze(bool freeze);
+void tft_display_toggle_page(void);
+i2c_master_bus_handle_t tft_display_get_i2c_bus(void);
 #endif
